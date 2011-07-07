ae4aaf44634ae7a1c35931149285ff56dcbd23fe
seraguira@gmail.com <seraguira@gmail.com@a7f31f19-8e2a-8840-c081-2db82d90b819>
2011-07-07 19:21:27 +0000

    Pagina Web Soldig en NetBeans

---
+++ REQM/src/java/war/Roles.java
@@ -0,0 +1,143 @@
+package war;
+
+import conexion.Conexion;
+import dao.DAOPFisicas;
+import dao.DAORoles;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLXML;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import libs.UserManager;
+import libs.XMLModder;
+
+public class Roles extends HttpServlet {
+
+    private String path = "C:/Users/Moncho/Documents/NetBeansProjects/REQM/web/";
+    //private String path = "/home/bluefox/NetBeansProjects/REQM/web/";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            HttpSession session = request.getSession(false);
+            UserManager user;
+            if (!session.isNew()) {
+                Conexion.autoConnect();
+                SQLXML personas = DAORoles.getXMLRecords();
+                user = (UserManager) session.getAttribute("user");
+                out.println(XMLModder.XSLTransform(
+                        XMLModder.JoinDocs(personas.getString(), user.getPermisos()), path + "../web/xsl/roles.xsl"));
+            } else {
+                Conexion.getConnection().disconnect();
+                request.getSession().invalidate();
+                response.sendRedirect("login.html");
+            }
+        } catch (Exception ex) {
+            //Logger.getLogger(ADM.class.getName()).log(Level.SEVERE, null, ex);
+            request.getSession(true).invalidate();
+            throw new ServletException("", ex);
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        PrintWriter out = response.getWriter();
+        try {
+            if (session.isNew() || session == null) {
+                Conexion.getConnection().disconnect();
+                session.invalidate();
+                response.sendRedirect("login.html");
+            } else {
+                response.setContentType("text/html;charset=UTF-8");
+                String mod = request.getParameter("mod"), nuevo = request.getParameter("ins");
+                if (mod == null) {
+                    if (nuevo == null) {
+                        processRequest(request, response);
+                    } else {
+                        UserManager user;
+                        request.setCharacterEncoding("UTF-8");
+                        Conexion.autoConnect();
+                        user = (UserManager) session.getAttribute("user");
+                        out.println(XMLModder.XSLTransform(
+                                XMLModder.JoinDocs(null, user.getPermisos()), path + "../web/xsl/roles_form.xsl"));
+                    }
+                } else {
+                    UserManager user;
+                    request.setCharacterEncoding("UTF-8");
+                    Conexion.autoConnect();
+                    SQLXML daopfisicas = DAORoles.getXMLRecords(mod);
+                    user = (UserManager) session.getAttribute("user");
+                    out.println(XMLModder.XSLTransform(
+                            XMLModder.JoinDocs(daopfisicas.getString(), user.getPermisos()), path + "../web/xsl/roles_form.xsl"));
+                }
+
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getStackTrace());
+            throw new ServletException(ex.getMessage(), ex.getCause());
+        } finally {
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        PrintWriter out = response.getWriter();
+        if (session.isNew()) {
+            session.invalidate();
+            response.sendRedirect("login.html");
+            return;
+        }
+        try {
+            response.setContentType("text/html;charset=UTF-8");
+            UserManager user;
+            request.setCharacterEncoding("UTF-8");
+            if (request.getParameter("ok.x") != null) {
+                Conexion.autoConnect();
+                DAORoles daorol = new DAORoles(new Integer(request.getParameter("inCode")), request.getParameter("inName"),new Integer(request.getParameter("inEstado")));
+                if (daorol.getiRolId() == 0) {
+                    daorol.insert();
+                } else {
+                    daorol.update();
+                }
+                response.sendRedirect("ok.html");
+            } else {
+                if (request.getParameter("del.x") != null) {
+                    Conexion.autoConnect();
+                    DAORoles daorol = new DAORoles();
+                    daorol.setiRolId(new Integer(request.getParameter("keycode")));
+                    daorol.delete();
+                    user = (UserManager) session.getAttribute("user");
+                    out.println(XMLModder.XSLTransform(
+                            XMLModder.JoinDocs(DAORoles.getXMLRecords().getString(), user.getPermisos()), path + "../web/xsl/roles.xsl"));
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getStackTrace());
+            throw new ServletException(ex.getMessage(), ex.getCause());
+        } finally {
+            out.close();
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "REQM System - Roles";
+    }// </editor-fold>
+}

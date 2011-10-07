85caba4aaf02c600336b3aeb9c2d1cb14a8cd7f1
seraguira@gmail.com <seraguira@gmail.com@a7f31f19-8e2a-8840-c081-2db82d90b819>
2011-10-07 20:21:42 +0000

    Nueva Iteracion Adicion de Modulo Monitor

--- REQM/src/java/dao/monitor/DAOEquipos.java
+++ REQM/src/java/dao/monitor/DAOEquipos.java
@@ -8,9 +8,11 @@ import java.sql.SQLXML;
 public class DAOEquipos extends DAO implements IDAO {
 //#Region VarDeclarations
 
-    public static final String tabla = "tblEquipos";
+    public static final String tabla = "tblMEquipos";
     public static final int F_LISTA = 1, F_REGISTRO = 2, F_NEW = 3, F_REPORT = 4;
     private long lEquipoId, lPersonaId;
+    private int iTipoFl,iRedFl;
+    private String sResponsableNm,sUsuarioNm,sUsuarioPass,sIPNm;
     private Date dtEquipoDt;
     //#End
 //#Region Builders
@@ -18,10 +20,15 @@ public class DAOEquipos extends DAO implements IDAO {
     public DAOEquipos() {
     }
 
-    public DAOEquipos(long lEquipoId, long lPersonaId, Date dtEquipoDt) {
+    public DAOEquipos(long lEquipoId, long lPersonaId, int iTipoFl, int iRedFl, String sResponsableNm, String sUsuarioNm, String sUsuarioPass, String sIPNm) {
         this.lEquipoId = lEquipoId;
         this.lPersonaId = lPersonaId;
-        this.dtEquipoDt = dtEquipoDt;
+        this.iTipoFl = iTipoFl;
+        this.iRedFl = iRedFl;
+        this.sResponsableNm = sResponsableNm;
+        this.sUsuarioNm = sUsuarioNm;
+        this.sUsuarioPass = sUsuarioPass;
+        this.sIPNm = sIPNm;
     }
 
     //#End
@@ -38,14 +45,14 @@ public class DAOEquipos extends DAO implements IDAO {
         return getProcessXML("gxml_", tabla, new Object[]{lPersonaId, filter});
     }
 
-    public static SQLXML searchXML(String value) throws SQLException {
-        return getProcessXML("srch_", tabla, new Object[]{"%" + value + "%"});
+    public static SQLXML searchXML(long lPersonaId,String value) throws SQLException {
+        return getProcessXML("srch_", tabla, new Object[]{lPersonaId,"%" + value + "%"});
     }
     //#End
 //#Region IUD
 
     public boolean insert() throws SQLException {
-        this.lEquipoId = insertRecord(tabla, new Object[]{lPersonaId});
+        this.lEquipoId = insertRecord(tabla, new Object[]{lPersonaId,iTipoFl,sResponsableNm,sUsuarioNm,sUsuarioPass,sIPNm,iRedFl});
         return true;
     }
 
@@ -82,5 +89,54 @@ public class DAOEquipos extends DAO implements IDAO {
     public void setlEquipoId(long lEquipoId) {
         this.lEquipoId = lEquipoId;
     }
+
+    public int getiRedFl() {
+        return iRedFl;
+    }
+
+    public void setiRedFl(int iRedFl) {
+        this.iRedFl = iRedFl;
+    }
+
+    public int getiTipoFl() {
+        return iTipoFl;
+    }
+
+    public void setiTipoFl(int iTipoFl) {
+        this.iTipoFl = iTipoFl;
+    }
+
+    public String getsIPNm() {
+        return sIPNm;
+    }
+
+    public void setsIPNm(String sIPNm) {
+        this.sIPNm = sIPNm;
+    }
+
+    public String getsResponsableNm() {
+        return sResponsableNm;
+    }
+
+    public void setsResponsableNm(String sResponsableNm) {
+        this.sResponsableNm = sResponsableNm;
+    }
+
+    public String getsUsuarioNm() {
+        return sUsuarioNm;
+    }
+
+    public void setsUsuarioNm(String sUsuarioNm) {
+        this.sUsuarioNm = sUsuarioNm;
+    }
+
+    public String getsUsuarioPass() {
+        return sUsuarioPass;
+    }
+
+    public void setsUsuarioPass(String sUsuarioPass) {
+        this.sUsuarioPass = sUsuarioPass;
+    }
+    
     //#End
 }

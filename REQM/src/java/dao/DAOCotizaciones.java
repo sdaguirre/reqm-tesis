5833433ba5ba8538da20492bfbdb718a6e0d50cc
seraguira@gmail.com <seraguira@gmail.com@a7f31f19-8e2a-8840-c081-2db82d90b819>
2011-10-26 15:10:37 +0000

    Nueva Iteracion Adicion de Modulo Monitor

---
+++ REQM/src/java/dao/DAOCotizaciones.java
@@ -0,0 +1,165 @@
+package dao;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLXML;
+import org.apache.commons.fileupload.FileItem;
+
+public class DAOCotizaciones extends DAO implements IDAO {
+//#Region VarDeclarations
+
+    public static final String tabla = "tblCotizaciones";
+    public static final int F_LISTA = 1, F_DOCUMENTO = 2, F_NEW = 3;
+    private long lReqmDocumentoId, lFReqmId;
+    private int iEstadoFl;
+    private Date dtReqmDocumentoDt;
+    private String sReqmDocumentoNm, sReqmDocumentoExt,sReqmDocumentoMIME;
+    private FileItem bnReqmDocumentoData;
+    //#End
+//#Region Builders
+
+    public DAOCotizaciones() {
+    }
+
+    public DAOCotizaciones(long lCotizacionId, long lPersonaId,Date dtCotizacionDt,String sCotizacionNm, FileItem bnCotizacionData) {
+        this.lReqmDocumentoId = lCotizacionId;
+        this.lFReqmId = lPersonaId;
+        this.dtReqmDocumentoDt = dtCotizacionDt;
+        this.sReqmDocumentoNm = sCotizacionNm;
+        if(bnCotizacionData!=null){
+            String[] pathf = bnCotizacionData.getName().split("\\.");
+            this.sReqmDocumentoExt = pathf[pathf.length - 1];
+            this.sReqmDocumentoMIME=bnCotizacionData.getContentType();
+        }
+        this.bnReqmDocumentoData = bnCotizacionData;
+    }
+
+    //#End
+//#Region SearchAndRecords
+    public static SQLXML getXMLRecords(long lKeyId, int filter) throws SQLException {
+        return getProcessXML("gxml_", tabla, new Object[]{lKeyId, filter});
+    }
+
+    public byte[] getDocument(long lCotizacionId) throws SQLException {
+        byte[] doc=null;
+        ResultSet rs = getProcessRecords("file_", tabla, new Object[]{lCotizacionId});
+        if (rs.next()) {
+            dtReqmDocumentoDt = rs.getDate(1);
+            sReqmDocumentoNm = rs.getString(2);
+            sReqmDocumentoExt = rs.getString(3);
+            sReqmDocumentoMIME = rs.getString(4);
+            doc=rs.getBytes(5);
+            return doc;
+        } else {
+            return null;
+        }
+    }
+
+    public static SQLXML search(String tabla,String value) throws SQLException {
+        return searchXMLRecords(tabla, "%" + value + "%");
+    }
+    //#End
+//#Region IUD
+
+    @Override
+    public boolean insert() throws SQLException {
+        this.lReqmDocumentoId = insertRecord(tabla, new Object[]{lFReqmId, sReqmDocumentoNm, sReqmDocumentoExt,sReqmDocumentoMIME, bnReqmDocumentoData});
+        return true;
+    }
+
+    @Override
+    public boolean update() throws SQLException {
+        return updateRecord(tabla, new Object[]{lReqmDocumentoId, iEstadoFl,lFReqmId, dtReqmDocumentoDt, sReqmDocumentoNm});
+    }
+    
+    public boolean upload() throws SQLException {
+        return updateRecord(tabla, new Object[]{lReqmDocumentoId,sReqmDocumentoExt,sReqmDocumentoMIME,bnReqmDocumentoData});
+    }
+
+    @Override
+    public boolean delete() throws SQLException {
+        return deleteRecord(tabla, lReqmDocumentoId);
+    }
+
+    //#End
+//#Region Accessors
+
+   public FileItem getBnReqmDocumentoData() {
+        return bnReqmDocumentoData;
+    }
+
+    public void setBnReqmDocumentoData(FileItem bnReqmDocumentoData) {
+        this.bnReqmDocumentoData = bnReqmDocumentoData;
+    }
+
+    public Date getDtReqmDocumentoDt() {
+        return dtReqmDocumentoDt;
+    }
+
+    public void setDtReqmDocumentoDt(Date dtReqmDocumentoDt) {
+        this.dtReqmDocumentoDt = dtReqmDocumentoDt;
+    }
+
+    public long getlFReqmId() {
+        return lFReqmId;
+    }
+
+    public void setlFReqmId(long lFReqmId) {
+        this.lFReqmId = lFReqmId;
+    }
+
+    public long getlReqmDocumentoId() {
+        return lReqmDocumentoId;
+    }
+
+    public void setlReqmDocumentoId(long lReqmDocumentoId) {
+        this.lReqmDocumentoId = lReqmDocumentoId;
+    }
+
+    public String getsReqmDocumentoExt() {
+        return sReqmDocumentoExt;
+    }
+
+    public void setsReqmDocumentoExt(String sReqmDocumentoExt) {
+        this.sReqmDocumentoExt = sReqmDocumentoExt;
+    }
+
+    public String getsReqmDocumentoMIME() {
+        return sReqmDocumentoMIME;
+    }
+
+    public void setsReqmDocumentoMIME(String sReqmDocumentoMIME) {
+        this.sReqmDocumentoMIME = sReqmDocumentoMIME;
+    }
+
+    public String getsReqmDocumentoNm() {
+        return sReqmDocumentoNm;
+    }
+
+    public void setsReqmDocumentoNm(String sReqmDocumentoNm) {
+        this.sReqmDocumentoNm = sReqmDocumentoNm;
+    }
+
+    public int getiEstadoFl() {
+        return iEstadoFl;
+    }
+
+    public void setiEstadoFl(int iEstadoFl) {
+        this.iEstadoFl = iEstadoFl;
+    }
+    
+    public String createDocName(){
+        String doc="";
+        if(dtReqmDocumentoDt!=null){
+            doc+=dtReqmDocumentoDt.toString()+"-";
+        }
+        String[] word = sReqmDocumentoNm.toUpperCase().split(" ");
+        for (String string : word) {
+            doc+=string.charAt(0);
+        }
+        doc+="."+sReqmDocumentoExt;
+        return doc;
+    }
+    //#End
+}
---
+++ REQM/src/java/war/monitor/Cotizaciones.java
@@ -0,0 +1,219 @@
+package war.monitor;
+
+import conexion.Conexion;
+import dao.DAOCotizaciones;
+import dao.DAOPFisicas;
+import dao.DAOObservaciones;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLXML;
+import java.util.HashMap;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import libs.UserManager;
+import libs.XMLModder;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+public class Cotizaciones extends HttpServlet {
+
+    private String path = "C:/Users/Moncho/Documents/NetBeansProjects/REQM/web/";
+    //private String path = "/home/bluefox/NetBeansProjects/REQM/web/";
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            HttpSession session = request.getSession(false);
+            UserManager user;
+            if (!session.isNew()) {
+                Conexion.autoConnect();
+                String keycode = request.getParameter("fkey");
+                if (keycode != null) {
+                    user = (UserManager) session.getAttribute("user");
+                    if (user.isbClient()) {
+                        session.setAttribute("PersonaId", user.getPersonaId());
+                    } else {
+                        session.setAttribute("PersonaId", new Long(keycode));
+                    }
+                    SQLXML proyectos = DAOCotizaciones.getXMLRecords(new Long(keycode), DAOCotizaciones.F_LISTA);
+                    out.println(XMLModder.XSLTransform(
+                            XMLModder.JoinDocs(proyectos.getString(),new String[]{user.getPermisos(),DAOObservaciones.getXMLRecords(user.getUsuarioId(),DAOObservaciones.F_NOTIFY).getString()}), path + "xsl/monitor/cotizaciones2.xsl"));
+                }  else {
+                    user = (UserManager) session.getAttribute("user");
+                    if (user.isbClient()) {
+                        out.println(XMLModder.XSLTransform(
+                                XMLModder.JoinDocs(DAOCotizaciones.getXMLRecords(user.getPersonaId(), DAOCotizaciones.F_LISTA).getString(),new String[]{user.getPermisos(),DAOObservaciones.getXMLRecords(user.getUsuarioId(),DAOObservaciones.F_NOTIFY).getString()}), path + "xsl/monitor/cotizaciones2.xsl"));
+                    } else {
+                        SQLXML personas = DAOPFisicas.getXMLRecords();
+                        out.println(XMLModder.XSLTransform(
+                                XMLModder.JoinDocs(personas.getString(),new String[]{user.getPermisos(),DAOObservaciones.getXMLRecords(user.getUsuarioId(),DAOObservaciones.F_NOTIFY).getString()}), path + "xsl/monitor/cotizaciones.xsl"));
+                    }
+                }
+            } else {
+                Conexion.getConnection().disconnect();
+                request.getSession().invalidate();
+                response.sendRedirect("login.html");
+            }
+        } catch (Exception ex) {
+            //Logger.getLogger(ADM.class.getName()).log(Level.SEVERE, null, ex);
+            request.getSession(true).invalidate();
+            throw new ServletException("", ex);
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        try {
+            if (session.isNew() || session == null) {
+                Conexion.getConnection().disconnect();
+                session.invalidate();
+                response.sendRedirect("login.html");
+            } else {
+                response.setContentType("text/html;charset=UTF-8");
+                String mod = request.getParameter("mod"), nuevo = request.getParameter("ins"), down = request.getParameter("down"), upload = request.getParameter("upl");
+                if (mod != null) {
+                    PrintWriter out = response.getWriter();
+                    UserManager user;
+                    request.setCharacterEncoding("UTF-8");
+                    Conexion.autoConnect();
+                    SQLXML cotizacion = DAOCotizaciones.getXMLRecords(new Long(mod), DAOCotizaciones.F_DOCUMENTO);
+                    user = (UserManager) session.getAttribute("user");
+                    out.println(XMLModder.XSLTransform(
+                            XMLModder.JoinDocs(cotizacion.getString(), new String[]{user.getPermisos(),}), path + "xsl/monitor/cotizaciones_form.xsl"));
+                } else if (nuevo != null) {
+                    PrintWriter out = response.getWriter();
+                    UserManager user;
+                    request.setCharacterEncoding("UTF-8");
+                    Conexion.autoConnect();
+                    user = (UserManager) session.getAttribute("user");
+                    out.println(XMLModder.XSLTransform(
+                            XMLModder.JoinDocs(DAOCotizaciones.getXMLRecords((Long) session.getAttribute("PRId"), DAOCotizaciones.F_NEW).getString(),
+                            new String[]{user.getPermisos()}), path + "xsl/monitor/cotizaciones_form.xsl"));
+                } else if (down != null) {
+                    DAOCotizaciones cotizacion = new DAOCotizaciones();
+                    byte[] doc = cotizacion.getDocument(new Long(down));
+                    if (doc != null) {
+                        response.setHeader("Content-Disposition", "attachment; filename=\"" + cotizacion.createDocName() + "\"");
+                        response.setHeader("cache-control", "no-cache");
+                        response.setContentType(cotizacion.getsReqmDocumentoMIME());
+                        response.getOutputStream().write(doc, 0, doc.length);
+                        response.getOutputStream().flush();
+                        response.getOutputStream().close();
+                    }
+                } else if (upload != null) {
+                    PrintWriter out = response.getWriter();
+                    UserManager user;
+                    request.setCharacterEncoding("UTF-8");
+                    Conexion.autoConnect();
+                    user = (UserManager) session.getAttribute("user");
+                    out.println(XMLModder.XSLTransform(
+                            XMLModder.JoinDocs("<root><key>"+upload+"</key></root>",new String[]{user.getPermisos()}), path + "xsl/monitor/cotizaciones_form.xsl"));
+                } else {
+                    processRequest(request, response);
+                }
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ServletException(ex.getMessage(), ex.getCause());
+        } finally {
+
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        PrintWriter out = response.getWriter();
+        if (session.isNew()) {
+            session.invalidate();
+            response.sendRedirect("login.html");
+            return;
+        }
+        try {
+            response.setContentType("text/html;charset=UTF-8");
+            UserManager user;
+//            request.setCharacterEncoding("UTF-8");
+            if (request.getParameter("del.x") != null) {
+                Conexion.autoConnect();
+                DAOCotizaciones cotizacion = new DAOCotizaciones();
+                cotizacion.setlReqmDocumentoId(new Long(request.getParameter("keycode")));
+                cotizacion.delete();
+                user = (UserManager) session.getAttribute("user");
+                out.println(XMLModder.XSLTransform(
+                        XMLModder.JoinDocs(DAOCotizaciones.getXMLRecords((Long) session.getAttribute("PRId"), DAOCotizaciones.F_LISTA).getString(),new String[]{user.getPermisos(),
+                        DAOObservaciones.getXMLRecords(user.getUsuarioId(),DAOObservaciones.F_NOTIFY).getString()}), path + "xsl/monitor/cotizaciones2.xsl"));
+            } else {
+                HashMap<String, Object> hash = new HashMap<String, Object>();
+                DiskFileItemFactory factory = new DiskFileItemFactory();
+                factory.setSizeThreshold(4096);
+                factory.setRepository(new File("C:/"));
+                ServletFileUpload upload = new ServletFileUpload(factory);
+                upload.setSizeMax(5242880);
+                List<FileItem> items = upload.parseRequest(request);
+                for (FileItem item : items) {
+                    if (item.isFormField()) {
+                        hash.put(item.getFieldName(), item.getString());
+                    } else {
+                        hash.put(item.getFieldName(), item);
+                    }
+                }
+                if (hash.get("ok.x") != null) {
+                    Conexion.autoConnect();
+                    DAOCotizaciones cotizacion = new DAOCotizaciones(new Long((String) hash.get("inCode")), (Long) session.getAttribute("PRId"), null, (String) hash.get("inName"), (FileItem) hash.get("inFile"));
+                    if (cotizacion.getlReqmDocumentoId() == 0) {
+                        cotizacion.insert();
+                    } else if (cotizacion.getlFReqmId()!=0 && cotizacion.getsReqmDocumentoNm()!=null){
+                        cotizacion.setiEstadoFl(new Integer(request.getParameter("inState")));
+                        cotizacion.update();
+                    }else{
+                        cotizacion.upload();
+                    }
+
+                    response.sendRedirect("ok.html");
+                }
+            }
+        } catch(FileUploadException fex){
+            response.sendRedirect("big.html");
+//            throw new ServletException(fex.getMessage(), fex.getCause());
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ServletException(ex.getMessage(), ex.getCause());
+        } finally {
+            out.close();
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "REQM System - Cotizaciones";
+    }// </editor-fold>
+}
